740252294f74ce6af5caaad5a545a4ad6eaf100d
Juan Faricelli <juan.faricelli@outlook.com>
2022-01-30 18:00:09 -0300

    items container component & responsive view corrections

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -7,14 +7,17 @@ import mockData from './item.json';
 import ItemDetailsShort from '../ItemDetailsShort';
 
 const Item = () => {
-  const { image_url: imageUrl, name, ibu, target_og: targetOg, abv } = mockData;
+  const { image_url: imageUrl, name, tagline, ibu, target_og: targetOg, abv } = mockData;
 
   return (
     <div className="item" data-testid="item">
       <img src={imageUrl} alt={name} />
-      <div className="item__name">{name}</div>
-      <ItemDetailsShort {...{ ibu, targetOg, abv }} />
-      <div className="button">Beer Detail</div>
+      <div className="item__details">
+        <div className="item__name">{name}</div>
+        <ItemDetailsShort {...{ ibu, targetOg, abv }} />
+        <div className="item__tagline">{tagline}</div>
+        <div className="button">Beer Detail</div>
+      </div>
     </div>
   );
 };
---
+++ src/components/ItemsContainer/ItemsContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+import Item from '../Item';
+
+const ItemsContainer = () => (
+  <div className="items-container" data-testid="items-container">
+
+  </div>
+);
+
+export default ItemsContainer;
