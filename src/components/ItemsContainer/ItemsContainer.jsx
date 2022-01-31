af4af55c6af4f6afcb6e24597cb27113b52181c9
Juan Faricelli <juan.faricelli@outlook.com>
2022-01-31 03:36:42 -0300

    WIP: sorting bar component

--- src/components/ItemsContainer/ItemsContainer.jsx
+++ src/components/ItemsContainer/ItemsContainer.jsx
@@ -1,19 +1,33 @@
 /* eslint-disable object-curly-newline */
-import React, { useState } from 'react';
-
-import './styles.scss';
+import React, { useState, useEffect, useContext } from 'react';
 
 import Item from '../Item';
 import ItemDetailsModal from '../ItemDetailsModal';
+import SortingBar from '../SortingBar';
+import { ItemsContext } from '../../api/context/itemContext';
+
+import './styles.scss';
 
 const ItemsContainer = ({ itemList }) => {
   const [showModal, setShowModal] = useState(false);
+  const [itemsList, setItemList] = useState(itemList || []);
+  const [sortedList, setSortedList] = useState(false);
+
+  useEffect(() => {
+    setItemList(itemList);
+    setSortedList(false);
+  }, [itemList, sortedList]);
 
   return (
-    <div className="items-container" data-testid="items-container">
-      {itemList.map((item) => <Item key={item.id} itemData={item} setShowModal={setShowModal} />)}
-      <ItemDetailsModal {...{ showModal, setShowModal }} />
-    </div>
+    <>
+      <SortingBar collection={itemsList} setItemList={setItemList} setSortedList={setSortedList} />
+      <div className="items-container" data-testid="items-container">
+        {itemsList.map(
+          (item) => <Item key={item.id} itemData={item} setShowModal={setShowModal} />
+        )}
+        <ItemDetailsModal {...{ showModal, setShowModal }} />
+      </div>
+    </>
   );
 };
 
